7a9f5baa885e4549bae7b95c01ad21b9dbc17bb4
tratorzim <161599875+tratorzim@users.noreply.github.com>
2024-10-16 09:45:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é javascript?',
+    'O javascript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da italia?',
+    'A capital da italia é roma'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Geografia',
+    'qual a cidade mais populosa do paraná?',
+    'Em números absolutos, a cidade que mais cresceu foi Curitiba que, não à toa, é também a mais populosa do Estado.'
+)
